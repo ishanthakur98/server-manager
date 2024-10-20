df93abec186def2bbe8f7a658f3a564172b91991
ishan thakur <ishan1998thakur@gmail.com>
2024-10-20 23:29:48 +0530

    added form control

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,7 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit,OnDestroy{
+  currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+  private interval?: ReturnType<typeof setInterval>;
+  constructor(){}
+  ngOnDestroy(): void {
+    clearTimeout(this.interval);
+  }
 
+  ngOnInit(){
+   this.interval = setInterval(() => {
+      const rd = Math.random();
+      if(rd < 0.5){
+        this.currentStatus = 'online';
+      }else if(rd <0.9){
+        this.currentStatus = 'offline';
+      }else{
+        this.currentStatus = 'unknown';
+      }
+    }, 10000);
+  }
 }
--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, inject, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,9 +8,17 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host:{
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
   @Input({required: true}) label! : string;
+  private el = inject(ElementRef);
+
+  onClick(){
+    // console.log("clicked");
+    // console.log(this.el);
+  }
+
 }
